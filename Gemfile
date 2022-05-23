689094264552595860553b00703e6dc772c2ee1c
Fabrizio Saullo <fabriziodsaullo@gmail.com>
2022-05-23 17:27:30 -0300

    game_controller finished

--- Gemfile
+++ Gemfile
@@ -47,6 +47,8 @@ group :test do
   gem 'selenium-webdriver', '>= 4.0.0.rc1'
   # Easy installation and use of web drivers to run system tests with browsers
   gem 'webdrivers'
+  # gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'pry-byebug'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,34 @@
 # Games Controller
+require 'open-uri'
+
 class GamesController < ApplicationController
   def new
+    @letters = ('a'..'z').to_a.sample(10)
   end
 
   def score
+    if include?
+      if valid_word?
+        @result = 'A palavra é válida!'
+      else
+        @result = 'A palavra não é válida!'
+      end
+    else
+      @result = 'Possui letras erradas!'
+    end
+  end
+
+  def include?
+    @each_letter = params[:letter].split
+    @word = params[:word].downcase
+    @word.chars.all? do |letter|
+      @word.count(letter) <= @each_letter.count(letter)
+    end
+  end
+
+  def valid_word?
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{params[:word]}")
+    json = JSON.parse(response.read)
+    json['found']
   end
 end
